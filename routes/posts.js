c3ec959b28a8f535235b93fd03c70e811c01a142
Paulo Pires <taralloqueiroiz@gmail.com>
2013-12-24 15:23:02 -0200

    mudando nome da rota de posts e criando pagina de novo post

--- routes/posts.js
+++ routes/posts.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
 
 	app.get('/posts', posts.index);
 	app.get('/post/:id', posts.show);
-	app.get('/postar', authentication, posts.postar);
-	app.post('/post', authentication, posts.create);
+	app.get('/postar',  posts.postar);
+	app.post('/post',  posts.create);
 	app.get('/post/:id/editar', authentication, posts.edit);
 	app.put('/post/:id', authentication, posts.update);
 	app.del('/post/:id', authentication, posts.destroy);
