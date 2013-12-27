7ab0dbf8e0107d0bc36e92f3be3b30522beb7e80
Paulo Pires <taralloqueiroiz@gmail.com>
2013-12-26 23:12:23 -0200

    adicionando teste de signup

--- test/home.js
+++ test/home.js
@@ -1,5 +1,4 @@
 var app = require('../app');
-var should = require('should');
 var request = require('supertest')(app);
 
 describe('In Home Controller', function () {
@@ -11,6 +10,16 @@ describe('In Home Controller', function () {
 			   });
 	});
 
+	it('deve ir para rota / ao fazer POST /signup', function (done) {
+		var signup = { user: { name: "Usuário Teste", email: 'teste@teste.com', password: 'teste123' } };
+		request.post('/signup')
+				.send(signup)
+				.end(function(err, res){
+					res.headers.location.should.eql('/');
+					done();
+				});
+	});
+
 	it('deve ir para rota / ao fazer GET /logout', function (done) {
 		request.get('/logout')
 			   .end(function(err, res){
@@ -20,7 +29,7 @@ describe('In Home Controller', function () {
 	});
 
 	it('deve ir para rota / ao fazer POST /login', function (done) {
-		var login = { user: { email: 'teste@teste.com', password: 1233456 } };
+		var login = { user: { email: 'teste@teste.com', password: 'teste123' } };
 		request.post('/login')
 			   .send(login)
 			   .end(function(err, res){
