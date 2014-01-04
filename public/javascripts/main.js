1ab414dd8e6bd6befec7dbf2f079f00a5bf7a780
Paulo Pires <taralloqueiroiz@gmail.com>
2014-01-04 15:24:45 -0200

    adicionando edito HTML no form de inserção de post

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
-  $('#tags').tagsInput({
-    defaultText:'Adicionar tag',
-  });
+	//Efeito de tags para posts
+	$('#tags').tagsInput({
+		defaultText:'Adicionar tag',
+	});
+
+	//Editor para posts
+	$('#body').wysihtml5({
+		"font-styles": false,
+		"html": true,
+		"stylesheets": ["/stylesheets/bootstrap.wysihtml5.css"]
+	});
 });
